03e09c7ad18e5b42efb302af25064b74dae25c2e
DevenPerkins <deven.s.perkins@gmail.com>
2021-02-25 12:04:57 -0800

    round two, added win logic

--- src/App.js
+++ src/App.js
@@ -6,12 +6,44 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [null, null, null, null, null, null, null, null, null],
+      history: [],
+      x: "x",
+      o: "o",
     }
   }
+  
+  isWinX = () =>{
+  const {squares , x} = this.state
+  const winCombosX = [
+    [squares[0] === x  && squares[1] === x && squares[2] === x ],
+    [squares[3] === x  && squares[4] === x && squares[5] === x ],
+    [squares[6] === x  && squares[7] === x && squares[8] === x ],
+  
+    [squares[0] === x  && squares[3] === x && squares[6] === x ],
+    [squares[1] === x  && squares[4] === x && squares[7] === x ],
+    [squares[2] === x  && squares[5] === x && squares[8] === x ],
 
+    [squares[0] === x  && squares[4] === x && squares[8] === x ],
+    [squares[2] === x  && squares[4] === x && squares[6] === x ],
+    ]
+  }
+  isWinO = () =>{
+  const {squares , o } = this.state
+  const winCombosO = [
+      [squares[0] === o  && squares[1] === o && squares[2] === o ],
+      [squares[3] === o  && squares[7] === o && squares[8] === o ],
+    
+      [squares[0] === o  && squares[3] === o && squares[6] === o ],
+      [squares[1] === o  && squares[4] === o && squares[7] === o ],
+      [squares[2] === o  && squares[5] === o && squares[8] === o ],
+  
+      [squares[0] === o  && squares[4] === o && squares[8] === o ],
+      [squares[2] === o  && squares[4] === o && squares[6] === o ],
+      ]
+    }
   handleGamePlay = (index) => {
-    const { square } = this.state
+    const { squares } = this.state
     // 
     
   }
